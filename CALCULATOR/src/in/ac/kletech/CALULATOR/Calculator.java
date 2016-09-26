ca92a950a9cb1ddd5f3ab8771386ad7ccb3a12e1
user <user@hp>
2016-09-27 00:00:50 +0530

    PracticeCAlculator

---
+++ CALCULATOR/src/in/ac/kletech/CALULATOR/Calculator.java
@@ -0,0 +1,16 @@
+package in.ac.kletech.CALULATOR;
+
+public class Calculator {
+   int numSum(int iNUM1,int iNUM2){
+	   return (iNUM1 +  iNUM2);
+   }
+   int numSub(int iNUM1,int iNUM2){
+	   return (iNUM1 - iNUM2);
+   }
+   int numMul(int iNUM1,int iNUM2){
+	   return (iNUM1 *  iNUM2);
+   }
+   int numdiv(int iNUM1,int iNUM2){
+	   return (iNUM1 / iNUM2);
+   }
+}
---
+++ CALCULATOR/src/in/ac/kletech/CALULATOR/CalulatorDemo.java
@@ -0,0 +1,19 @@
+package in.ac.kletech.CALULATOR;
+
+public class CalulatorDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calculator Casio = new Calculator();
+		
+		System.out.println(Casio.numSum(10,40));
+		System.out.println(Casio.numSub(10,40));
+		System.out.println(Casio.numMul(20,10));
+		System.out.println(Casio.numdiv(6,2));
+		// TODO Auto-generated method stub
+
+	}
+
+}
